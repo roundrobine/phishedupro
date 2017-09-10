4d28fa0f5ecc234edf46807405557046ca7e31e2
roundrobine <roundrobine@gmail.com>
2017-09-10 04:39:04 +0200

    add new properties to MY_WOT constant and update category value from int to string

--- server/api/scan/scan.config.js
+++ server/api/scan/scan.config.js
@@ -6,6 +6,8 @@
 const MY_WOT = {
   TRUSTWORTHINESS: "0",
   CHILD_SAFETY: "4",
+  CATEGORIES: "categories",
+  BLACKLISTS: "blacklists",
   REPUTATION: {
     EXCELLENT: {
       NAME: "Excellent",
@@ -31,87 +33,87 @@ const MY_WOT = {
   CATEGORY:{
     C_101:{
       DESCRIPTION: "Malware or viruses",
-      VALUE: 101
+      VALUE: "101"
     },
     C_102:{
       DESCRIPTION: "Poor customer experience",
-      VALUE: 102
+      VALUE: "102"
     },
     C_103:{
       DESCRIPTION: "Phishing",
-      VALUE: 103
+      VALUE: "103"
     },
     C_104:{
       DESCRIPTION: "Scam",
-      VALUE: 104
+      VALUE: "104"
     },
     C_105:{
       DESCRIPTION: "Potentially illegal",
-      VALUE: 105
+      VALUE: "105"
     },
     C_201:{
       DESCRIPTION: "Misleading claims or unethical",
-      VALUE: 201
+      VALUE: "201"
     },
     C_202:{
       DESCRIPTION: "Privacy risks",
-      VALUE: 202
+      VALUE: "202"
     },
     C_203:{
       DESCRIPTION: "Suspicious",
-      VALUE: 203
+      VALUE: "203"
     },
     C_204:{
       DESCRIPTION: "Hate, discrimination",
-      VALUE: 204
+      VALUE: "204"
     },
     C_205:{
       DESCRIPTION: "Spam",
-      VALUE: 205
+      VALUE: "205"
     },
     C_206:{
       DESCRIPTION: "Potentially unwanted programs",
-      VALUE: 206
+      VALUE: "206"
     },
     C_207:{
       DESCRIPTION: "Ads / pop-ups",
-      VALUE: 207
+      VALUE: "207"
     },
     C_301:{
       DESCRIPTION: "Online tracking",
-      VALUE: 301
+      VALUE: "301"
     },
     C_302:{
       DESCRIPTION: "Alternative or controversial medicine",
-      VALUE: 302
+      VALUE: "302"
     },
     C_303:{
       DESCRIPTION: "Opinions, religion, politics",
-      VALUE: 303
+      VALUE: "303"
     },
     C_304:{
       DESCRIPTION: "Other",
-      VALUE: 304
+      VALUE: "304"
     },
     C_501:{
       DESCRIPTION: "Good site",
-      VALUE: 501
+      VALUE: "501"
     },
     C_401:{
       DESCRIPTION: "Adult content",
-      VALUE: 401
+      VALUE: "401"
     },
     C_402:{
       DESCRIPTION: "Incidental nudity",
-      VALUE: 402
+      VALUE: "402"
     },
     C_403:{
       DESCRIPTION: "Gruesome or shocking",
-      VALUE: 403
+      VALUE: "403"
     },
     C_404:{
       DESCRIPTION: "Site for kids",
-      VALUE: 404
+      VALUE: "404"
     }
   },
   BLACKLIST_TYPE:{
@@ -141,12 +143,7 @@ const MY_WOT = {
 
 module.exports = {
 
-  // MongoDB connection options
-  mongo: {
-    uri: 'mongodb://localhost/phishedupro-dev'
-  },
-
-  // Seed database on startup
-  seedDB: true
+  // My_WOT Config variable
+  MY_WOT: MY_WOT
 
 };
