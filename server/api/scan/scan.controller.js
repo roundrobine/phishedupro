3ab031e587c33ea8b20b8aa22eec9b8eb823af9c
roundrobine <roundrobine@gmail.com>
2017-10-25 03:12:07 +0200

    log and print database error on create new website scan object

--- server/api/scan/scan.controller.js
+++ server/api/scan/scan.controller.js
@@ -47,6 +47,7 @@ function isUrlIPAddress(url){
 function handleError(res, statusCode) {
   statusCode = statusCode || 500;
   return function(err) {
+    console.log(err);
     res.status(statusCode).send(err);
   };
 }
