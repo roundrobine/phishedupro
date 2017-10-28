f4da31868d9f9fb37228179e9d1eddf3c166b15d
roundrobine <roundrobine@gmail.com>
2017-10-28 21:08:36 +0200

    implement initial scan website results on the frontend

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -7,13 +7,64 @@
     vm.isAdmin = Auth.isAdmin;
     vm.currentUser = currentUser;
     vm.scan = {url:"", target:null};
+    vm.scanResults = {};
+
+    const PHISHING_CLASS = {
+      LEGITIMATE: 1,
+      SUSPICIOUS: 0,
+      PHISHING: -1
+    }
+    const PHISHING_CLASS_TEXT = {
+      LEGITIMATE: "Legitimate",
+      SUSPICIOUS: "Suspicious",
+      PHISHING: "Phishing"
+    }
 
 
     vm.scanUrl = function () {
       vm.scan.owner = vm.currentUser._id;
       URLScanFactory.save(vm.scan, function (scanReport) {
         console.log(scanReport);
+        vm.scanResults = scanReport;
+      }, function(error) {
+        console.log(error);
       });
+    };
+
+    vm.applyCollor = function (value) {
+      let phishingClass = "";
+      switch (value){
+        case PHISHING_CLASS.LEGITIMATE:
+          phishingClass = "badge-success"
+          break;
+        case PHISHING_CLASS.SUSPICIOUS:
+          phishingClass = "badge-warning"
+          break;
+        case PHISHING_CLASS.PHISHING:
+          phishingClass = "badge-danger"
+          break;
+        default:
+          phishingClass = "badge-info"
+      }
+      return phishingClass;
+    };
+
+    vm.phishingClass = function (value) {
+      let phishingClass = "";
+      switch (value){
+        case PHISHING_CLASS.LEGITIMATE:
+          phishingClass = PHISHING_CLASS_TEXT.LEGITIMATE
+          break;
+        case PHISHING_CLASS.SUSPICIOUS:
+          phishingClass = PHISHING_CLASS_TEXT.SUSPICIOUS
+          break;
+        case PHISHING_CLASS.PHISHING:
+          phishingClass = PHISHING_CLASS_TEXT.PHISHING
+          break;
+        default:
+          phishingClass = "Unknown"
+      }
+      return phishingClass;
     };
 
   };
